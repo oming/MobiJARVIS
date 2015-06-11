ec1ea8daea8c301bf9faf9eb3e0a486392e12620
hyosang,An <oming.kr@gmail.com>
2015-06-12 02:42:03 +0900

    komoran2.0 파일 추가

--- app/src/main/java/com/example/anhyosang/mobijarvis/MainActivity.java
+++ app/src/main/java/com/example/anhyosang/mobijarvis/MainActivity.java
@@ -2,36 +2,65 @@ package com.example.anhyosang.mobijarvis;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
     Intent i;
     SpeechRecognizer mRecognizer;
     TextView tv;
-
+    MorphemeAnalysis ma;
+    TextView tvr;
+    String path = "file:///android_asset/models/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         startActivity(new Intent(this, SplashActivity.class)); // 미리 시작함
+//        Uri newpath = Uri.parse("file:///android_asset/models/models-full");
+
+
+
+            ma = new MorphemeAnalysis(path);
+            Log.d("Assset11111111", path);
+
+//        Log.d("Assset", newpath.getPath());
+
         setContentView(R.layout.activity_main);
+
+//        AssetManager assetMgr = getAssets();
+//        try {
+//            String list[] = assetMgr.list(path);
+//            if(list != null) {
+//                for(int i=0; i<list.length; ++i) {
+//                    Log.d("Assets:", list[i]);
+//                }
+//            }
+//        } catch (IOException e) {
+//            Log.d("list error", "con't list" + path);
+//            e.printStackTrace();
+//        }
 
         // 안드로이드 STT 기능 사용
 
         i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         i.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
         i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko_KR");
-
 
 
     }
@@ -53,13 +82,17 @@ public class MainActivity extends Activity {
 
         @Override
         public void onResults(Bundle results) {
-            tv = (TextView )findViewById(R.id.tv);
+            tv = (TextView) findViewById(R.id.tv);
             String key = "";
             key = SpeechRecognizer.RESULTS_RECOGNITION;
             ArrayList<String> mResult = results.getStringArrayList(key);
             String[] rs = new String[mResult.size()];
             mResult.toArray(rs);
-            tv.setText(""+rs[0]);
+            tv.setText("" + rs[0]);
+
+            List<String> ls = ma.getParsing(rs[0]);
+            tvr = (TextView) findViewById(R.id.textViewResult);
+            tvr.setText(ls.toString());
         }
 
         @Override
--- app/src/main/java/com/example/anhyosang/mobijarvis/MorphemeAnalysis.java
+++ app/src/main/java/com/example/anhyosang/mobijarvis/MorphemeAnalysis.java
@@ -1,5 +1,7 @@
 package com.example.anhyosang.mobijarvis;
 
+import android.content.res.AssetManager;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,128 +10,124 @@ import kr.co.shineware.util.common.model.Pair;
 
 /**
  * 형태소 분석을 위한 클래스 메시지를 입력받아 형태소를 분석하여 상황에 맞는 명령어를 실행시킬수 있도록 하는 클래스.
- * 
- * @author hyosang
  *
+ * @author hyosang
  */
 public class MorphemeAnalysis {
-//	Komoran komoran;
-//	RunningApplications ra;
-//	RequestGoogleTextToSpeech rgtts;
+    Komoran komoran;
+
+    public MorphemeAnalysis(String path) {
+        komoran = new Komoran(path+"/models-full");
+    }
+
+    public List<String> getParsing(String message) {
+        List<String> mList = new ArrayList<String>();
+
+        List<List<Pair<String, String>>> result = komoran.analyze(message);
+
+        for (List<Pair<String, String>> eojeolResult : result) {
+            for (Pair<String, String> wordMorph : eojeolResult) {
+                if (wordMorph.getSecond().equals("NNG") || wordMorph.getSecond().equals("NNP"))
+                    // System.out.println(wordMorph.getFirst() + "//" +
+                    // wordMorph.getSecond());
+                    mList.add(wordMorph.getFirst());
+            }
+        }
+        return mList;
+    } // end getParsing
+
+//    public void analysis(List<String> list) {
+//        loop:
+//        for (String s : list) {
+//            switch (s) {
+//                case "실행":
+//                    // 실행 분기 실행
+//                    list.remove(s); // 현재 검색된 내용을 삭제함
+//                    for (String b : list) {
+//                        switch (b) {
+//                            case "브라우저":
+//                            case "인터넷":
+//                                ra.runWebBrowser();
+//                                rgtts.TTSPlayer("브라우저를 실행합니다.");
+//                                break loop;
 //
-//	public MorphemeAnalysis() {
-//		komoran = new Komoran("./models/models-full/");
-//		ra = new RunningApplications();
-//		rgtts = new RequestGoogleTextToSpeech();
-//	}
+//                            case "메모장":
+//                                ra.runMemoPad();
+//                                rgtts.TTSPlayer("메모장을 실행합니다.");
+//                                break loop;
 //
-//	public List<String> getParsing(String message) {
-//		List<String> mList = new ArrayList<String>();
+//                            case "음악":
+//                            case "노래":
+//                                ra.runMusicPlayer();
+//                                rgtts.TTSPlayer("음악을 실행합니다.");
+//                                break loop;
 //
-//		List<List<Pair<String, String>>> result = komoran.analyze(message);
+//                            case "카카오톡":
+//                            case "카톡":
+//                                ra.runKaKaoTalk();
+//                                rgtts.TTSPlayer("카카오톡을 실행합니다.");
+//                                break loop;
 //
-//		for (List<Pair<String, String>> eojeolResult : result) {
-//			for (Pair<String, String> wordMorph : eojeolResult) {
-//				if (wordMorph.getSecond().equals("NNG") || wordMorph.getSecond().equals("NNP"))
-//					// System.out.println(wordMorph.getFirst() + "//" +
-//					// wordMorph.getSecond());
-//					mList.add(wordMorph.getFirst());
-//			}
-//		}
-//		return mList;
-//	} // end getParsing
+//                            case "계산기":
+//                                ra.runCalculator();
+//                                rgtts.TTSPlayer("계산기를 실행합니다.");
+//                                break loop;
+//                        } // end switch
+//                    } // end for
+//                    break;
+//                // 실행 분기 종료
 //
-//	public void analysis(List<String> list) {
-//		loop: for (String s : list) {
-//			switch (s) {
-//			case "실행":
-//				// 실행 분기 실행
-//				list.remove(s); // 현재 검색된 내용을 삭제함
-//				for (String b : list) {
-//					switch (b) {
-//					case "브라우저":
-//					case "인터넷":
-//						ra.runWebBrowser();
-//						rgtts.TTSPlayer("브라우저를 실행합니다.");
-//						break loop;
+//                case "검색":
+//                    // 검색 분기 시작
+//                    list.remove(s); // 현재 검색된 내용을 삭제함
 //
-//					case "메모장":
-//						ra.runMemoPad();
-//						rgtts.TTSPlayer("메모장을 실행합니다.");
-//						break loop;
+//                    for (String b : list) {
+//                        switch (b) {
+//                            case "네이버":
+//                                list.remove(b); // 현재 검색된 내용을 삭제함
+//                                ra.runWebBrowser("http://search.naver.com/search.naver?ie=utf8&where=nexearch&query=", list.get(0));
+//                                rgtts.TTSPlayer("네이버에서 " + list.get(0) + "를 검색합니다.");
+//                                break loop;
+//                            case "구글":
+//                                list.remove(b);// 현재 검색된 내용을 삭제함
+//                                ra.runWebBrowser("http://www.google.co.kr/search?q=", list.get(0));
+//                                rgtts.TTSPlayer("구글에서 " + list.get(0) + "를 검색합니다.");
+//                                break loop;
+//                            case "다음":
+//                                break loop;
+//                            default:
+//                                System.out.println("오류남.");
+//                        } // end switch
+//                    } // end for
 //
-//					case "음악":
-//					case "노래":
-//						ra.runMusicPlayer();
-//						rgtts.TTSPlayer("음악을 실행합니다.");
-//						break loop;
+//                    break;
+//                // 검색 분기 종료
 //
-//					case "카카오톡":
-//					case "카톡":
-//						ra.runKaKaoTalk();
-//						rgtts.TTSPlayer("카카오톡을 실행합니다.");
-//						break loop;
+//                case "자비스":
+//                    // 호출 분기 시작
+//                    for (String b : list) {
+//                        switch (b) {
+//                            case "안녕":
+//                                list.remove(b); // 현재 검색된 내용을 삭제함
+//                                rgtts.TTSPlayer("예스 마스터 안녕하세요.");
+//                                break loop;
+//                            case "뭐해":
+//                                list.remove(b);// 현재 검색된 내용을 삭제함
+//                                rgtts.TTSPlayer("대기 중입니다.");
+//                                break loop;
 //
-//					case "계산기":
-//						ra.runCalculator();
-//						rgtts.TTSPlayer("계산기를 실행합니다.");
-//						break loop;
-//					} // end switch
-//				} // end for
-//				break;
-//			// 실행 분기 종료
+//                            default:
+//                                rgtts.TTSPlayer("예스 마스터!");
+//                                break loop;
+//                        } // end switch
+//                    } // end for
 //
-//			case "검색":
-//				// 검색 분기 시작
-//				list.remove(s); // 현재 검색된 내용을 삭제함
+//                    break;
+//                // 호출 분기 종료.
+//                default:
+//                    System.out.println("명령어 실행 오류");
+//            } // close switch
+//        } // close for
 //
-//				for (String b : list) {
-//					switch (b) {
-//					case "네이버":
-//						list.remove(b); // 현재 검색된 내용을 삭제함
-//						ra.runWebBrowser("http://search.naver.com/search.naver?ie=utf8&where=nexearch&query=", list.get(0));
-//						rgtts.TTSPlayer("네이버에서 " + list.get(0) + "를 검색합니다.");
-//						break loop;
-//					case "구글":
-//						list.remove(b);// 현재 검색된 내용을 삭제함
-//						ra.runWebBrowser("http://www.google.co.kr/search?q=", list.get(0));
-//						rgtts.TTSPlayer("구글에서 " + list.get(0) + "를 검색합니다.");
-//						break loop;
-//					case "다음":
-//						break loop;
-//					default:
-//						System.out.println("오류남.");
-//					} // end switch
-//				} // end for
-//
-//				break;
-//			// 검색 분기 종료
-//
-//			case "자비스":
-//				// 호출 분기 시작
-//				for (String b : list) {
-//					switch (b) {
-//					case "안녕":
-//						list.remove(b); // 현재 검색된 내용을 삭제함
-//						rgtts.TTSPlayer("예스 마스터 안녕하세요.");
-//						break loop;
-//					case "뭐해":
-//						list.remove(b);// 현재 검색된 내용을 삭제함
-//						rgtts.TTSPlayer("대기 중입니다.");
-//						break loop;
-//
-//					default:
-//						rgtts.TTSPlayer("예스 마스터!");
-//						break loop;
-//					} // end switch
-//				} // end for
-//
-//				break;
-//			// 호출 분기 종료.
-//			default:
-//				System.out.println("명령어 실행 오류");
-//			} // close switch
-//		} // close for
-//
-//	} // end analysis
+//    } // end analysis
 }
