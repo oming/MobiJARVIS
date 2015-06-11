12a9eb23bf9e72f2bec46f8db81e0935a1554555
hyosang,An <oming.kr@gmail.com>
2015-06-11 21:55:46 +0900

    불필요 파일 제거 및 아이콘 변경

--- app/src/main/java/com/example/anhyosang/mobijarvis/MainActivity.java
+++ app/src/main/java/com/example/anhyosang/mobijarvis/MainActivity.java
@@ -20,8 +20,6 @@ public class MainActivity extends Activity {
     TextView tv;
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
